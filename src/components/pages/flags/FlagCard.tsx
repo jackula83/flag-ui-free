ddfd80c213c444af27b7bfd9927c85a2bebbe379
jackula83 <jackula@gmail.com>
2022-08-21 20:56:27 +1000

    added text fade exceeding 3 lines

--- src/components/pages/flags/FlagCard.tsx
+++ src/components/pages/flags/FlagCard.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Flag } from '../../../operations/queries/flag'
+import styled from 'styled-components';
 
 interface IProps {
   flag: Flag
 }
+
+// http://jsfiddle.net/b9vtW/4/
 
 const FlagCard: React.FC<IProps> = ({flag}) => {
   const onButton = <div className="btn btn-gradient-success btn-rounded">On</div>
   const offButton = <div className="btn btn-gradient-light btn-rounded">Off</div>
   return (
     <>
+    <Wrapper>
       <div className="card">
         <div className="card-body py-3">
           <div className="row">
@@ -23,8 +27,34 @@ const FlagCard: React.FC<IProps> = ({flag}) => {
           </div>
         </div>
       </div>
+      </Wrapper>
     </>
   )
 }
+
+const Wrapper = styled.div`
+  .card {
+    overflow: hidden;
+  }
+
+  p {
+    content: '';
+    overflow: hidden;    
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical; 
+  }
+
+  p:after {
+    content: '';
+    bottom: 0;
+    right: 0;
+    width: 100%;
+    height: 25%;
+    background: linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1) 70%);
+    /* /* background: linear-gradient(transparent 50px, white); */
+    position: absolute;
+  }
+`
 
 export default FlagCard
